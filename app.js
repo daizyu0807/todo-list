e8e8964c37420d452f6a0d0fff5708e076f62096
daizyu0807 <daizyu0807@gmail.com>
2022-01-14 14:41:44 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -66,6 +66,14 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`APP is running on port:${port}`)
 })
